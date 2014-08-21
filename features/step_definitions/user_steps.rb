fde02c8a185d7002f44f9a2c763dd23d8994f48f
Yaro Apletov <yaro@mail.ru>
2014-08-21 13:20:40 +0100

    crated a scenario and steps

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -2,8 +2,8 @@ Given /^I have an avatar image at "([^"]*)"$/ do |link|
   @avatar_link = link
 end
 
-Given /^I am logged in as user with email "([^"]*)", with password "([^"]*)"$/ do |email, password|
-  @user = FactoryGirl.create(:user, email: email, password: password, password_confirmation: password)
+Given /^I am logged in as user with (?:name "([^"]*)", )?email "([^"]*)", with password "([^"]*)"$/ do |name, email, password|
+  @user = FactoryGirl.create(:user, first_name: name, email: email, password: password, password_confirmation: password)
   visit new_user_session_path
   within ('#main') do
     fill_in 'user_email', :with => email
@@ -370,3 +370,10 @@ end
 When(/^My email receivings is set to false$/) do
   @user.update_attribute(:receive_mailings, false)
 end
+
+When(/^I delete my profile$/) do 
+  @user.delete
+end
+
+
+
